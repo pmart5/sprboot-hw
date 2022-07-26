7dd5093135c3a5ed472653cb288e228881560aa5
Пётр Мартынчик <pmart5a@yandex.ru>
2022-07-26 19:05:28 +0400

    Удалён комментарий из SystemProfile

--- src/main/java/com/pmart5a/sprboothw/service/SystemProfile.java
+++ src/main/java/com/pmart5a/sprboothw/service/SystemProfile.java
@@ -3,9 +3,3 @@ package com.pmart5a.sprboothw.service;
 public interface SystemProfile {
     String getProfile();
 }
-
-//@FunctionalInterface
-//public interface SystemProfile {
-//
-//    String getProfile();
-//}
